33449e60746bdcddd9ace245b3bdc57a00f3872d
Serge Kamga <serge_h.kamga@yahoo.com>
2020-12-19 21:25:00 -0800

    added a form with a textfield for the check amount and also a picker to select the number of people

--- check-splitting-app/ContentView.swift
+++ check-splitting-app/ContentView.swift
@@ -8,9 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 2
+    
+    let tipPercentages = [10,15,20,25,0]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<20) { number in
+                            Text("\(number) people")
+                        }
+                    }
+                }
+                
+            }.navigationBarTitle("Split the bill")
+        }
     }
 }
 
