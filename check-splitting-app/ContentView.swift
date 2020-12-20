7e374869c7468dd0e46239bd415ece872ca3b243
Serge Kamga <serge_h.kamga@yahoo.com>
2020-12-19 20:46:09 -0800

    first commit

---
+++ check-splitting-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  check-splitting-app
+//
+//  Created by Serge Kamga on 12/19/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
